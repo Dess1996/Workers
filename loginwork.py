fe32d81cde418e555083eaf2e541f130fac58657
Dess1996 <mihail96-96@mail.ru>
2023-04-10 12:24:45 +0500

    Добавил хеширование пароля

--- loginwork.py
+++ loginwork.py
@@ -27,4 +27,4 @@ if __name__ == '__main__':
 	usr.LoginGet('sue')
 	usr.LoginGet('mama')
 	usr.PasswordUpdate('1hrtyt222333', '4e1d072006')
-	print(Workers)
+	print(Workers) # Output: {'dess': {'login': 'dess', 'password': # b'$2b$12$mX65cK73RNhQHVb.cj8lPOn4Um0LHMRN/e88vPozQXqafHvVk8zY.'}}
